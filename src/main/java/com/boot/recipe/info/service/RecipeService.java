7ae5267d83709a357a3da68fc79b01c79aef6cd5
halfvideo <half0512@gmail.com>
2023-03-17 15:08:54 +0900

    스토어 제품 데이터 입력
    스토어 제품 데이터 입력 크롤링

--- src/main/java/com/boot/recipe/info/service/RecipeService.java
+++ src/main/java/com/boot/recipe/info/service/RecipeService.java
@@ -1,9 +1,14 @@
 package com.boot.recipe.info.service;
 
+import java.util.List;
+
 import com.boot.recipe.info.vo.RecipeVO;
+import com.boot.recipe.ingredient.vo.IngredientVO;
 import com.boot.recipe.manual.vo.RecipeManualVO;
 
 public interface RecipeService {
 	public int insertRecipe(RecipeVO rvo);
 	public int insertRecipeManual(RecipeManualVO rvo);
+	
+	public List<RecipeVO> searchRecipe(IngredientVO ivo);
 }
